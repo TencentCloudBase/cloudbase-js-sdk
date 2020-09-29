85a1724f0ff6789f8c7478fa7e8d74dd2303333d
zhoujunpeng <zjp0432@163.com>
2020-09-29 10:57:10 +0800

    chore: build

--- packages/app/dist/cjs/constants/common.d.ts
+++ packages/app/dist/cjs/constants/common.d.ts
@@ -4,6 +4,7 @@ export declare function setSdkName(name: string): void;
 export declare function getSdkName(): string;
 export declare const DATA_VERSION = "2020-01-10";
 export declare function setEndPoint(url: string, protocol?: 'http' | 'https'): void;
+export declare function setRegionLevelEndpoint(env: string, region: string, protocol?: 'http' | 'https'): void;
 export declare function getEndPoint(): {
     BASE_URL: string;
     PROTOCOL: string;
